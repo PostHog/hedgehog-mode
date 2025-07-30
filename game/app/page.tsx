15eeb1d21727a739ab6a4ea62bd6e6fb51470f8e
Ben White <ben@posthog.com>
2025-07-30 20:32:32 +0200

    Fixes

--- game/app/page.tsx
+++ game/app/page.tsx
@@ -1,17 +1,14 @@
 "use client";
-import { HedgehogModeRenderer, HedgeHogMode } from "@posthog/hedgehog-mode";
-import { useState } from "react";
+import { HedgehogModeRenderer } from "@posthog/hedgehog-mode";
 
 export default function Home() {
-  const [game, setGame] = useState<HedgeHogMode | null>(null);
-
   return (
     <div>
       <HedgehogModeRenderer
         config={{
           assetsUrl: "/assets",
         }}
-        onGameReady={setGame}
+        onGameReady={() => {}}
       />
     </div>
   );
